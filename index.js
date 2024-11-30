ec541d3607d963bb3837a42153aeb9d8aff89cea
Lina Svensson <lina.kuzmanowsky@gmail.com>
2024-11-30 20:00:53 +0100

    added a second html page for the quiz

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const themeToggleBtn = document.getElementById("themeToggleBtn");
 const outerWrapper = document.querySelector(".outer-wrapper");
+const footer = document.querySelector("footer");
 
 //Check for saved theme in localStorage
 const currentTheme = localStorage.getItem("theme");
 if (currentTheme === "dark") {
     outerWrapper.classList.add("dark-mode");
+    footer.classList.add("dark-mode");
     themeToggleBtn.textContent = "Switch: Light Mode";
 }
 
 //Toggle theme on button click
 themeToggleBtn.addEventListener("click", () => {
     const isDarkMode = outerWrapper.classList.toggle("dark-mode");
+    footer.classList.toggle("dark-mode");
 
     //Update button text
     themeToggleBtn.textContent = isDarkMode ? "Switch: Light Mode" : "Switch: Dark Mode";
@@ -18,3 +21,7 @@ themeToggleBtn.addEventListener("click", () => {
     //Save the current theme to localStorage
     localStorage.setItem("theme", isDarkMode ? "dark" : "light");
 });
+
+document.getElementById("ctaBtn").addEventListener("click", () => {
+    window.location.href = "quiz.html"; // Navigate to quiz.html
+  });
