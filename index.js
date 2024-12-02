7fae6157486330456a5d6d40fed72c30b38c5177
Lina Svensson <lina.kuzmanowsky@gmail.com>
2024-12-02 17:09:08 +0100

    added data correct

--- index.js
+++ index.js
@@ -21,7 +21,33 @@ themeToggleBtn.addEventListener("click", () => {
     //Save the current theme to localStorage
     localStorage.setItem("theme", isDarkMode ? "dark" : "light");
 });
-
+//check if quiz button exist in DOM
+const ctaBtn = document.getElementById("ctaBtn");
+if (ctaBtn) {
 document.getElementById("ctaBtn").addEventListener("click", () => {
     window.location.href = "quiz.html"; // Navigate to quiz.html
+  });
+}
+
+  //submit the quiz
+  document.querySelector("#submit").addEventListener("click", (event) => {
+    event.preventDefault();
+    let score = 0;
+
+    const radioInputs = document.querySelectorAll("input[type='radio']:checked");
+    radioInputs.forEach(input => {
+        if (input.dataset.correct === "true") {
+            score++;
+        }
+    });
+
+    const checkboxInputs = document.querySelectorAll("input[type='checkbox']");
+    const selectedCheckboxes = [...checkboxInputs].filter(input => input.checked && input.dataset.correct === "true");
+    const correctCheckboxes = [...checkboxInputs].filter(input => input.dataset.correct === "true");
+
+    //kollar så båda checkboxarna är checkade
+    if (selectedCheckboxes.length === correctCheckboxes.length && correctCheckboxes.every(cb => cb.checked)){
+        score++;
+    }
+    alert(`Your score: ${score}`);
   });
