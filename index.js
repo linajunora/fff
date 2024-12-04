f328659ca8f25c22b1360cf462eb1a1d730a99cd
Lina Svensson <lina.kuzmanowsky@gmail.com>
2024-12-04 19:58:01 +0100

    added some styling to buttons and js to buttons

--- index.js
+++ index.js
@@ -1,2 +1,50 @@
+//theme toggle stuff
 const themeToggleBtn = document.getElementById('themeToggleBtn');
+let darkmode = localStorage.getItem('darkmode')
 
+const enableDarkmode = () => {
+    document.body.classList.add('darkmode')
+    localStorage.setItem('darkmode', 'active')
+}
+
+const disableDarkmode = () => {
+    document.body.classList.remove ('darkmode')
+    localStorage.setItem('darkmode', null)
+}
+
+if(darkmode === "active") enableDarkmode()
+
+themeToggleBtn.addEventListener("click", () => {
+    darkmode = localStorage.getItem('darkmode')
+    darkmode !== "active" ? enableDarkmode() : disableDarkmode()
+});
+
+//quiz
+
+const fieldsets = Array.from(document.querySelectorAll('form fieldset'));
+console.log(fieldsets)
+const nextBtn = document.getElementById('nextBtn');
+const submitBtn = document.getElementById('submit');
+const form = document.getElementById('form-quiz');
+const wrapper = document.getElementById('fieldset-wrapper');
+
+let currentIndex = 0;
+
+const newFieldset = () => {
+    wrapper.innerHTML = '';
+    fieldsets.forEach((fieldset) => {
+        if (form.contains(fieldset)) {
+            form.removeChild(fieldset);
+        }
+    });
+    wrapper.appendChild(fieldsets[currentIndex]);
+}
+
+nextBtn.addEventListener('click', () => {
+    if (currentIndex < fieldsets.length -1){
+        currentIndex++;
+        newFieldset();
+    }
+});
+
+newFieldset();
